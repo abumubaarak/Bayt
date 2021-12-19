a73b0ba20ee1c58b773a4a1641fa0e24cb9fc5f4
ismail <ismaildeve070@gmail.com>
2021-12-19 03:38:21 +0100

    Added copyright link on the home page

--- client/src/pages/Home.page.tsx
+++ client/src/pages/Home.page.tsx
@@ -1,8 +1,9 @@
-import { Box, Container, VStack } from "@chakra-ui/layout";
-import React, { FC } from "react";
 import BackgroundImage from "@assets/background.jpg";
+import { Box, Container, VStack } from "@chakra-ui/layout";
+import { Text } from "@chakra-ui/react";
 import { HomePageProps } from "@type/base";
- 
+import React, { FC } from "react";
+
 const Home: FC<HomePageProps> = ({ children, header }) => {
    return (
       <Box
@@ -17,6 +18,15 @@ const Home: FC<HomePageProps> = ({ children, header }) => {
                {children}
             </VStack>
          </Container>
+         <a
+            href='https://dribbble.com/shots/14259769-Town-House/attachments/5908270?mode=media'
+            target='_blank'
+         >
+             <Text shadow="md" fontWeight="semibold" p={5} color='white' pos='fixed' bottom='0' right='0'>
+            copyright
+         </Text>
+         </a>
+        
       </Box>
    );
 };
