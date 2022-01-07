1ff8d339cba879f6c6a8ea4d5163e34047db561d
ismail <ismaildeve070@gmail.com>
2022-01-07 02:17:16 +0100

    Changed expose port

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,10 +1,10 @@
 const Redis = require("ioredis");
 
-//const redis = new Redis();
+const redis = new Redis();
 
 
-const redis = new Redis({
-  port: 6379,
-  host: "redis",
-});
+// const redis = new Redis({
+//   port: 6379,
+//   host: "redis",
+// });
 export {redis}
