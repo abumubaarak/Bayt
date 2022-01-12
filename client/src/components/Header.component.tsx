d49a5290261183c46795edc1693e00fa03ba302e
ismail <ismaildeve070@gmail.com>
2022-01-12 04:25:54 +0100

    fixing header issue

--- client/src/components/Header.component.tsx
+++ client/src/components/Header.component.tsx
@@ -44,7 +44,7 @@ const Header: FC<HeaderProps> = ({ variant }) => {
    const [type, setType] = useState<boolean>();
    const { onSubmit } = useSubmit();
    const { data } = useUser();
-   data?.data && localStorage.setItem("id", data?.data._id!);
+   localStorage.setItem("id", data?.data?._id!);
 
     const handleLogout = () => {
       logout();
--- client/src/hooks/axios.ts
+++ client/src/hooks/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance =
    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
       ? axios
-        : axios.create({ baseURL: process.env.BASE_URL_PROD });
-      
+      : axios.create({ baseURL: process.env.REACT_APP_BASE_URL_PROD });
 
-   export default instance;
+
+export default instance;
